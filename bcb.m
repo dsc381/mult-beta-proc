a5bbde73860721f83c40609e5339eaeacf4e0f82
dsc381 <dcohen@cs.umass.edu>
2015-11-15 20:54:41 -0500

    working direct SGD

--- bcb.m
+++ bcb.m
@@ -1,7 +1,7 @@
 qtok = strsplit(q,' ');
 %move to better spot later
 num_col = size(index);
-tol = 1e-1;
+tol = 1e-5;
 %end
 score = zeros(length(doclengths),1);
 for i = 1:length(qtok)
@@ -15,18 +15,18 @@ for i = 1:length(qtok)
     B =ones(ind(2)-ind(1)+1,2)*.5;
     %implement counts with doc # = indices, val = number of terms
     tf = [tf_q doclengths((index(ind(1):ind(2))+1),1)-tf_q];
-    error = 1e1;
+    error = [10 10];
+    prev = [0 0];
     %gradient descent for B
-    while error > tol
-        denom1 = psi([tf(:,1)+sum(B,2),tf(:,2)+sum(B,2)]);
-        denom2 = psi(sum(B,2));
-        denom3 = [denom1(:,1)-denom2,denom1(:,2)-denom2];
-        B_old = B;
-        B = B.*(psi(tf+B)-psi(B))./(denom3);
-        error = max(abs(B-B_old));
+    while max(error > tol) && isequal(error,prev)
+        prev = error;
+        denom = [(psi(sum(tf,2)+sum(B,2)) - psi(sum(B,2))), (psi(sum(tf,2)+sum(B,2)) - psi(sum(B,2)))];
+        B_old=B;
+        B = B.*(psi(tf+B)-psi(B))./denom;
+        error = max(B-B_old);
     end
     %make sure this is correct in terms of element wise matrix
-    score((index(ind(1):ind(2))+1),1) = score((index(ind(1):ind(2))+1),1) + sum((tf-1 .* B + (tf-1).*(tf)./2.))';
+    score((index(ind(1):ind(2))+1),1) = score((index(ind(1):ind(2))+1),1) + sum((tf-1 .* B + (tf-1).*(tf)./2.),2);
 end
     
    
