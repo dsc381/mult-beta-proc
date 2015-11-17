cf31d7fdb09b3a01c5893ec1e4de18af3bfb8e0b
dsc381 <dcohen@cs.umass.edu>
2015-11-17 13:53:03 -0500

    batch processing for index

--- index_2_mat.py
+++ index_2_mat.py
@@ -1,16 +1,31 @@
 import os,sys
+import os
+
+import os
+import psutil
+
+process = psutil.Process(os.getpid())
+mem = psutil.virtual_memory()
+ram = mem.total  # total physical memory available
+GB = 2**30
+cutoff = (ram*.001)/GB
+print cutoff
 f = open(sys.argv[1],'r')
+q = open('postings_m','w')
 converted = []
 row = 0
 for line in f:
     row += 1
+    if (row%1000 == 0 and process.memory_info().rss/(2**20) > cutoff):
+        print "clearing ram"
+        q.write(''.join(converted))
+        converted = []
     line = line[:-1].split(',')
     col = 0
     for entry in line[1:]:
         col+=1
         converted.append(str(row)+'\t'+str(col)+'\t'+entry+'\n')
 f.close()
-f = open('postings_m','w')
-f.write(''.join(converted))
+q.write(''.join(converted))
 
 
