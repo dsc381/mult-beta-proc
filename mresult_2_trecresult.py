91b42f6c2d57b03b2cbae8306cfcf02df4554151
dsc381 <dcohen@cs.umass.edu>
2015-11-20 15:11:36 -0500

    added script to convert results for trec eval

---
+++ mresult_2_trecresult.py
@@ -0,0 +1,17 @@
+num_name = {}
+with open ('names') as f:
+    for l in f:
+        l = l.split(',')
+        l[1] = l[1].split('/')[-1]
+        l[1] = l[1].split('.')[0]
+        num_name[int(l[0])] = l[1][:-1]
+with open('matlab_output.csv') as f:
+    trec_results = []
+    for line in f:
+        print line
+        line = line.split(',')
+        q = line[0]
+        k = 40.
+        for i,r in enumerate(line[1:]):
+            trec_results.append(' '.join([q,num_name[int(r)],'1',str(k-i),'bp']))
+print '\n'.join(trec_results)
