7f1f5a4ebb5ccbe2939595683be73c6f15b62ac2
dsc381 <dcohen@cs.umass.edu>
2015-11-20 15:34:59 -0500

    same

--- mresult_2_trecresult.py
+++ mresult_2_trecresult.py
@@ -8,11 +8,10 @@ with open ('names') as f:
 with open('matlab_output.csv') as f:
     trec_results = []
     for line in f:
-        print line
         line = line.split(',')
         q = line[0]
         k = 40.
         for i,r in enumerate(line[1:]):
-            trec_results.append(' '.join([q,num_name[int(r)],'1',str(k-i),'bp']))
+            trec_results.append(' '.join([q,'0',num_name[int(r)],'1',str(k-i),'bp']))
 q = open('m_results.eval','w')
 q.write('\n'.join(trec_results))
