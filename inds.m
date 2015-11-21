ca074cad535c30923e4a764bf403a0a0671f48c7
dsc381 <dcohen@cs.umass.edu>
2015-11-21 03:14:14 -0500

    proper indenting

--- inds.m
+++ inds.m
@@ -1,16 +1,14 @@
 function [indices] = inds(index,ind,mode)
-disp('ind')
 if strcmp(mode,'tf')
     indices = zeros(ind(2)-ind(1),1);
-k = 1;
-for i=ind(1):ind(2)
-    indices(k,1) = sum(index(index(:,1) == i,3).' ~= -1);
-    k = k+1;
-end
+    k = 1;
+    for i=ind(1):ind(2)
+        indices(k,1) = sum(index(index(:,1) == i,3).' ~= -1);
+        k = k+1;
+    end
 end
 if strcmp(mode,'doc')
     indices = index( ind(1) <=index(:,1) & index(:,1) <= ind(2) & index(:,2) == 1,3)+1;
 end
-disp('ind')
 
 
