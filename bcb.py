e6ab74df31c5643dfd824c249f8c38af91092e27
dsc381 <dcohen@cs.umass.edu>
2015-11-23 03:28:07 -0500

    working converter and removes nonrel docs

--- bcb.py
+++ bcb.py
@@ -2,7 +2,9 @@ import numpy as np
 from scipy.special import psi
 from scipy import sparse
 from sys import getsizeof
+import cPickle as pickle
 import csv
+import os
 
 # def look_up_doc_tf(begin,end):
 #     terms = []
@@ -28,17 +30,32 @@ def index_in():
                 pass
         return i + 1
 
-    size = file_len('postings_m')
-    index = np.zeros([size,3])
-    with open('postings_m') as f:
-        for i,l in enumerate(f):
-            index[i,:] = map(int,l.split('\t'))
-    # index = np.loadtxt('postings_m',dtype=np.uint32)
-    print ('finished reading in postings')
-    index = sparse.csc_matrix((index[:,2], (index[:,0]-1,index[:,1]-1)) ,dtype=np.uint32)
-    print ('finished sparse')
-    data = np.loadtxt('lengths',delimiter = ',',usecols=(1,))
-    doclengths = np.reshape(data,[len(data),1])
+    if 'index.p' not in os.listdir('./'):
+        size = file_len('postings_m')
+        index = np.zeros([size,3])
+        with open('postings_m') as f:
+            for i,l in enumerate(f):
+                index[i,:] = map(int,l.split('\t'))
+        print ('finished reading in postings')
+        # index = np.loadtxt('postings_m',dtype=np.uint32)
+        index = sparse.csc_matrix((index[:,2], (index[:,0]-1,index[:,1]-1)) ,dtype=np.uint32)
+        f = open('index.p','wb')
+        pickle.dump(index,f)
+    else:
+        f = open('index.p','r')
+        index = pickle.load(f)
+    f.close()
+    print ('finished loading sparse')
+    if 'lengths.p' not in os.listdir('./'):
+        data = np.loadtxt('lengths',delimiter = ',',usecols=(1,))
+        # doclengths = np.reshape(data,[len(data),1])
+        f = open('lengths.p','wb')
+        pickle.dump(data,f)
+    else:
+        f = open('lengths.p','r')
+        data = pickle.load(f)
+    f.close()
+
     print("finished reading lengths")
     #build map with key -> begin,end
 
@@ -47,13 +64,23 @@ def index_in():
     return index,data
 
 def mapping():
+    if 'map.p' in os.listdir('./'):
+        f = open('map.p','r')
+        m = pickle.load(f)
+        f.close()
+        print('loaded mapping')
+        return m
     fin_id = -1
     idd=0
     m = {}
+    run = 0
     with open('postings') as f:
         line1 = f.readline()
         line2 = f.readline()
         while(line1):
+	    run += 1
+            if run % 100000 == 0:
+		print run,
             word1 = line1.split(',')[0]
             word2 = line2.split(',')[0]
             fin_id = fin_id+1
@@ -68,6 +95,10 @@ def mapping():
             line1 = line2
             line2 = f.readline()
         m[word1] = [start,fin_id+1]
+    print "done"
+    f = open('map.p','wb')
+    pickle.dump(m,f)
+    f.close()
     return m
 
 def bcb(qtok,index,doclengths,m):
@@ -97,9 +128,15 @@ def bcb(qtok,index,doclengths,m):
         score[used_docs.T] = score[used_docs.T] + np.sum((tf-1 * B + (tf-1)*(tf)/2.),axis=1)
     n = 40
     print ''
-    return score.argsort()[::-1][:n]
+    final = score.argsort()[::-1][:n]
+    for i,idx in enumerate(final):
+        if score[idx] == 0:
+            return final[:i]
+
 
 def evaluate(fil_name):
+    index,doclengths = index_in()
+    m = mapping()
     f = open(fil_name,'r')
     queries = f.readlines()
     results = np.zeros([len(queries),41])
--- mresult_2_trecresult.py
+++ mresult_2_trecresult.py
@@ -4,14 +4,15 @@ with open ('names') as f:
         l = l.split(',')
         l[1] = l[1].split('/')[-1]
         l[1] = l[1].split('.')[0]
-        num_name[int(l[0])] = l[1][:-1]
-with open('matlab_output.csv') as f:
+        num_name[float(l[0])] = l[1][:-1]
+with open('python_results') as f:
     trec_results = []
     for line in f:
-        line = line.split(',')
+        print line
+        line = line.split(' ')
         q = line[0]
         k = 40.
         for i,r in enumerate(line[1:]):
-            trec_results.append(' '.join([q,'0',num_name[int(r)],'1',str(k-i),'bp']))
-q = open('m_results.eval','w')
+            trec_results.append(' '.join([q,'0',num_name[float(r)],'1',str(k-i),'bp']))
+q = open('bcb_results.eval','w')
 q.write('\n'.join(trec_results))
